22df638ab05595ed9583ace038979149a2f71951
Liam Yates <liampy@bu.edu>
2025-11-16 15:51:07 -0500

    fully working version on my local machine, pushing to ensure that I have this to go back to if cloning down into main somehow messes it up

--- client/src/app/authentication/page.tsx
+++ client/src/app/authentication/page.tsx
@@ -1,13 +1,17 @@
 "use client";
 
 import { useEffect } from "react";
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 
 export default function AuthPage() {
   const router = useRouter();
 
   useEffect(() => {
     // Redirect to student login page
+    const searchParams = useSearchParams();
+    console.log("redirecting to login.")
+    console.log("search params: ");
+    console.log(searchParams);
     router.push("/login");
   }, [router]);
 
